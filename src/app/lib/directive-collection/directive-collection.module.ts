f082dbb0da2ec628cbd1f556709a3ba75121f42c
adityad <adityad@portall.in>
2023-05-03 11:38:22 +0530

    directive for number only

--- src/app/lib/directive-collection/directive-collection.module.ts
+++ src/app/lib/directive-collection/directive-collection.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatepickerDirective } from './datepicker/datepicker.directive';
 import { Datepicker2Directive } from './datepicker2/datepicker2.directive';
+import { NumberonlyDirective } from './numberonly/numberonly.directive';
 
 
 
 @NgModule({
   declarations: [
     DatepickerDirective,
-    Datepicker2Directive
+    Datepicker2Directive,
+    NumberonlyDirective
   ],
   imports: [
     CommonModule
   ],
-  exports:[DatepickerDirective,Datepicker2Directive]
+  exports:[DatepickerDirective,Datepicker2Directive,NumberonlyDirective]
 })
 export class DirectiveCollectionModule { }
