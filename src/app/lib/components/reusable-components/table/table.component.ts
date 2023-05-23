92e702476804310fadd9f90f93f05250d82b2043
adityad <adityad@portall.in>
2023-05-23 12:09:45 +0530

    table issue fix

--- src/app/lib/components/reusable-components/table/table.component.ts
+++ src/app/lib/components/reusable-components/table/table.component.ts
@@ -12,7 +12,7 @@ export class TableComponent implements OnInit, AfterViewInit {
 
   @Input() tableData: Array<any> = []
   @Input() tableColumn: Array<any> = []
-  @Input() tableKeys: Array<any> = []
+ 
   @Input() showViewBtn: boolean = false
   @Input() showEditBtn: boolean = false
   @Input() showDeleteBtn: boolean = false
@@ -21,42 +21,44 @@ export class TableComponent implements OnInit, AfterViewInit {
   @Output() onEdit: any = new EventEmitter<any>();
   @Output() onDelete: any = new EventEmitter<any>();
   DataKeys: Array<any> = []
+  columns: Array<any> = []
   constructor(private cdref: ChangeDetectorRef, private renderer: Renderer2) { }
   ngAfterViewInit(): void {
 
-  
+
     let that = this;
     $('#example').DataTable({
       data: this.tableData,
       destroy: true,
-      columns: [
-        { title: 'First Name', data: 'firstname' },
-        { title: 'Last Name', data: 'lastname' },
-        { title: 'Twitter.', data: 'twitter' },
-        {
-          title: 'Action',
-          data: 'firstname',
-          render: function (data, type, row) {
-            var html = ""
-            if (that.showViewBtn) {
-              html = html + '<button  class="btn btn-light m-1 view" data-row=' + JSON.stringify(row) + '>View</button>'
-            }
-            if (that.showViewBtn) {
-              html = html + '<button *ngIf="showEditBtn" data-row=' + JSON.stringify(row) + ' class="btn btn-primary m-1 edit" (click)="edit(' + row + ')">Edit</button>'
-            }
+      columns: this.columns
+      // [
+      //   { title: 'First Name', data: 'firstname' },
+      //   { title: 'Last Name', data: 'lastname' },
+      //   { title: 'Twitter.', data: 'twitter' },
+      //   {
+      //     title: 'Action',
+      //     data: 'firstname',
+      //     render: function (data, type, row) {
+      //       var html = ""
+      //       if (that.showViewBtn) {
+      //         html = html + '<button  class="btn btn-light m-1 view" data-row=' + JSON.stringify(row) + '>View</button>'
+      //       }
+      //       if (that.showViewBtn) {
+      //         html = html + '<button *ngIf="showEditBtn" data-row=' + JSON.stringify(row) + ' class="btn btn-primary m-1 edit" (click)="edit(' + row + ')">Edit</button>'
+      //       }
 
-            if (that.showViewBtn) {
-              html = html + '<button *ngIf="showDeleteBtn" data-row=' + JSON.stringify(row) + ' class="btn btn-danger m-1 delete"(click)="delete(' + row + ')">Delete</button>'
-            }
-            return html
+      //       if (that.showViewBtn) {
+      //         html = html + '<button *ngIf="showDeleteBtn" data-row=' + JSON.stringify(row) + ' class="btn btn-danger m-1 delete"(click)="delete(' + row + ')">Delete</button>'
+      //       }
+      //       return html
 
-          }
-        },
+      //     }
+      //   },
 
-      ],
+      // ],
     });
 
-   
+
     $('#example').on('click', 'button', function () {
       debugger
       if ($(this).hasClass('view')) {
@@ -74,58 +76,68 @@ export class TableComponent implements OnInit, AfterViewInit {
   }
   ngOnChanges(changes: SimpleChanges) {
     let that = this;
+    this.prepareDtOptions();
     if (changes.tableData.currentValue.length > 0) {
       $('#example').DataTable({
         data: this.tableData,
         destroy: true,
-        columns: [
-          // { title: 'Sr.No' },
-          { title: 'First Name', data: 'firstname' },
-          { title: 'Last Name', data: 'lastname' },
-          { title: 'Twitter.', data: 'twitter' },
-          {
-            title: 'Action',
-            data: 'firstname',
-            render: function (data, type, row, full) {
-              var html = ""
-              if (that.showViewBtn) {
-                html = html + '<button  class="btn btn-light m-1 view" data-row=' + JSON.stringify(row) + '>View</button>'
-              }
-              if (that.showViewBtn) {
-                html = html + '<button *ngIf="showEditBtn" data-row=' + JSON.stringify(row) + ' class="btn btn-primary m-1 edit" (click)="edit(' + row + ')">Edit</button>'
-              }
-
-              if (that.showViewBtn) {
-                html = html + '<button *ngIf="showDeleteBtn" data-row=' + JSON.stringify(row) + ' class="btn btn-danger m-1 delete"(click)="delete(' + row + ')">Delete</button>'
-              }
-              return html
-
-            }
-          },
-      
-        ],
+        columns: that.columns
       });
 
-    
+
     }
-   
+
   }
 
   ngOnInit(): void {
     if (this.tableData.length > 0) {
       this.DataKeys = Object.keys(this.tableData[0]);
     }
-   
+
 
 
 
   }
 
+  prepareDtOptions() {
+    debugger
+    let that = this;
+    this.columns = []
+    this.columns.push({
+      render: function (data:any, type:any, full:any, meta:any) {
+      
+        return meta.row + 1; 
+      }
+    }
+    )
+    this.columns = this.columns.concat(this.tableColumn)
+    this.columns.push(
+      {
+        title: 'Action',
+
+        render: function (data:any, type:any, row:any, full:any, meta:any) {
+          var html = ""
+          if (that.showViewBtn) {
+            html = html + '<button  class="btn btn-light m-1 view" data-row=' + JSON.stringify(row) + '>View</button>'
+          }
+          if (that.showEditBtn) {
+            html = html + '<button *ngIf="showEditBtn" data-row=' + JSON.stringify(row) + ' class="btn btn-primary m-1 edit" (click)="edit(' + row + ')">Edit</button>'
+          }
+
+          if (that.showDeleteBtn) {
+            html = html + '<button *ngIf="showDeleteBtn" data-row=' + JSON.stringify(row) + ' class="btn btn-danger m-1 delete"(click)="delete(' + row + ')">Delete</button>'
+          }
+          return html
+
+        }
+      })
+
+  }
   edit(item: any) {
     this.onEdit.emit(item)
   }
   view(item: any) {
-    
+
     this.onView.emit(item)
   }
   delete(item: any) {
--- src/app/modules/account/login/login.component.ts
+++ src/app/modules/account/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { LayoutService } from 'src/app/lib/services/layout-service/layout.service';
 import { StorageService } from 'src/app/lib/services/storage-service/storage.service';
+import { ApiHelperService } from '../../../lib/services/api-helpers/api-helper.service';
 
 @Component({
   selector: 'app-login',
@@ -10,34 +11,88 @@ import { StorageService } from 'src/app/lib/services/storage-service/storage.ser
 })
 export class LoginComponent implements OnInit {
 
-  tableColumn: Array<any> = ["Sr.No", "First Name", "Last Name", "Twitter"]
-  tableKeys: Array<any> = [ "firstname", "lastname", "twitter"]
-  tableData: Array<any> = [{ "firstname": "Aditya", "lastname": "Deokar", "twitter": "@adityadeokar" }, { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" }]
+  tableColumn: Array<any> = [
+    // { title: 'Sr.No' },
+    { title: 'First Name', data: 'firstname' },
+    { title: 'Last Name', data: 'lastname' },
+    { title: 'Twitter.', data: 'twitter' }]
+
+  tableKeys: Array<any> = ["firstname", "lastname", "twitter"]
+  tableData: Array<any> = [];
+  //= [{ "firstname": "Aditya", "lastname": "Deokar", "twitter": "@adityadeokar" }, { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" }]
   frm: any;
-  constructor(private fb : FormBuilder,private _layoutService: LayoutService, private storageService: StorageService) { }
+  constructor(private api: ApiHelperService, private fb: FormBuilder, private _layoutService: LayoutService, private storageService: StorageService) { }
 
   ngOnInit(): void {
+    // this.postApi();
+    // this.getApi();
     this.frm = this.fb.group({
-      date1:[""],
-      date2:[""]
+      date1: [""],
+      date2: [""]
     })
     this.frm.get('date2').setValue('2023-12-31 14:20')
-    // this.storageService.setLocalStorageItem("bacchi","Saurabh")
+    this.storageService.setLocalStorageItem("bacchi", "Saurabh")
+    this.tableData.length = 0
+    setTimeout(() => {
 
+      this.tableData = [
+        { "firstname": "Aditya", "lastname": "Deokar", "twitter": "@adityadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" },
+        { "firstname": "Rutuja", "lastname": "Deokar", "twitter": "@rutujadeokar" }
+      ]
+
+    }, 3000);
   }
-  submit(){
+  submit() {
     alert(JSON.stringify(this.frm.value))
   }
-  add()
-  {
+  add() {
     this.tableData.push({ "firstname": "Aditya", "lastname": "Deokar", "twitter": "@adityadeokar" })
   }
   view(item: any) {
     alert(JSON.stringify(item))
   }
   delete(item: any) {
-    
-    this.tableData.splice(this.tableData.indexOf(this.tableData.find(x=>x.firstname == item.firstname),1))
+
+    this.tableData.splice(this.tableData.indexOf(this.tableData.find(x => x.firstname == item.firstname), 1))
   }
   ngAfterContentInit(): void {
     this._layoutService.setConfig({ header: true, footer: true, sidebar: false })
@@ -46,7 +101,7 @@ export class LoginComponent implements OnInit {
   ngOnDestroy(): void {
     this._layoutService.resetConfig();
   }
-
+  
 
 
 }
